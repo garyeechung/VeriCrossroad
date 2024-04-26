6260d73f3755394eae62188104303bbff8e0b4ae
Gary Chung <garyeechung@outlook.com>
2024-04-26 16:52:19 -0500

    feat: add "adding new safe route"

--- src/solvers.py
+++ src/solvers.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from z3 import And, Ints, Not, Or, Solver, sat
 
 from .utils import convert_greenlights_to_routes, get_combinations
@@ -88,6 +90,91 @@ class Collision:
     
     def remove_greenlight(self, way: int, light: int):
         self.greenlights[way].remove(light)
+        self.reset_solver()
+
+
+class AddSafeRoute:
+    def __init__(self, greenlights, n_ways: int = None):
+        self.greenlights = greenlights
+        if n_ways is None:
+            self.n_ways = max(greenlights.keys()) + 1
+        else:
+            self.n_ways = n_ways
+
+        self.reset_solver()
+    
+    def reset_solver(self):
+        self.existing_routes = convert_greenlights_to_routes(self.greenlights, self.n_ways)
+        self.candidate_routes = (product(range(self.n_ways), range(self.n_ways)))
+        self.candidate_routes = [
+            (route[0], route[1])
+            for route in self.candidate_routes
+            if (route not in self.existing_routes) and (route[0] != route[1])
+        ]
+
+        self.solver = Solver()
+        self.s1, self.e1 = Ints('s1 e1')
+        self.solver.add(Or([And(self.s1 == route[0], self.e1 == route[1])
+                            for route in self.candidate_routes]))
+        
+        self.solver.add(And([self.get_not_collision_constraint(route[0], route[1])
+                             for route in self.existing_routes]))
+
+    
+    def get_not_collision_constraint(self, s2: int, e2: int):
+        # if s1 < s2
+        collision_constraints_1_1 = And(
+            self.s1 < s2,
+            (s2-self.s1)%self.n_ways < (self.e1-self.s1)%self.n_ways,
+            Or((self.e1-self.s1)%self.n_ways < (e2-self.s1)%self.n_ways, e2 == self.s1)
+        )
+        # if s1 > s2
+        collision_constraints_1_2 = And(
+            self.s1 > s2,
+            (self.s1-s2)%self.n_ways < (e2-s2)%self.n_ways,
+            Or((e2-s2)%self.n_ways < (self.e1-s2)%self.n_ways, self.e1 == s2)
+        )
+
+        # if e1 == s2
+        collision_constraints_2_1 = And(
+            self.e1 == s2,
+            (e2-self.s1)%self.n_ways > 0,
+            (e2-self.s1)%self.n_ways < (self.e1-self.s1)%self.n_ways
+        )
+        # if e2 == s1
+        collision_constraints_2_2 = And(
+            e2 == self.s1,
+            (self.e1-s2)%self.n_ways > 0,
+            (self.e1-s2)%self.n_ways < (e2-s2)%self.n_ways
+        )
+
+        totol_collision_constraints = Or(
+            collision_constraints_1_1,
+            collision_constraints_1_2,
+            collision_constraints_2_1,
+            collision_constraints_2_2
+        )
+        return Not(totol_collision_constraints)
+    
+    def check_safe_route(self, verbose=False):
+        safe_routes = []
+        if verbose:
+            print("Checking safe route...")
+        while self.solver.check() == sat:
+            sol = self.solver.model()
+            start = sol[self.s1].as_long()
+            end = sol[self.e1].as_long()
+            safe_routes.append((start, end))
+            if verbose:
+                print(f"{start} -> {end}")
+            self.solver.add(Not(And(
+                self.s1 == sol[self.s1],
+                self.e1 == sol[self.e1]
+            )))
+        return safe_routes
+    
+    def add_safe_route(self, start: int, end: int):
+        self.greenlights[start].append((end - start) % self.n_ways)
         self.reset_solver()
 
 
